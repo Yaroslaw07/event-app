ad98c583e623aecc004444994af3f96dc18ee88e
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2023-10-06 12:04:16 +0200

    added header and event detail

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,4 @@
-import AddressIcon from "../icons/adress-icon";
+import AddressIcon from "../icons/address-icon";
 import ArrowRightIcon from "../icons/arrow-right-icon";
 import DateIcon from "../icons/date-icon";
 import Button from "../ui/button";
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,36 @@
-function EventDetailPage () {
-    return (
-        <div>
-            <h1>Event Detail</h1>
-        </div>
-    )
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import { getEventById } from "@/data/dummy_data";
+import EventContent from "@/components/events/event-content";
+import EventSummary from "@/components/events/event-summary";
+import EventLogistics from "@/components/events/event-logistics";
+
+
+function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.date}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
 
 export default EventDetailPage;
